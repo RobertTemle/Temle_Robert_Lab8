3b0d4e6eee67f31680b2ad52e3037ed8221dd3ad
Robert Samuel Temle <Temle.Va.Robert@student.utcluj.ro>
2021-11-27 14:58:03 +0200

    Add project files.

---
+++ Data/Temle_Robert_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Temle_Robert_Lab8.Models;
+
+namespace Temle_Robert_Lab8.Data
+{
+    public class Temle_Robert_Lab8Context : DbContext
+    {
+        public Temle_Robert_Lab8Context (DbContextOptions<Temle_Robert_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Temle_Robert_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Temle_Robert_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Temle_Robert_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Temle_Robert_Lab8.Data;
+using Temle_Robert_Lab8.Models;
+
+namespace Temle_Robert_Lab8.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Temle_Robert_Lab8.Data.Temle_Robert_Lab8Context _context;
+
+        public DetailsModel(Temle_Robert_Lab8.Data.Temle_Robert_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
